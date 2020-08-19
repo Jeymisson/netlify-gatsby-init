86c29da682b9b7621640ec7d92f04d4550257d5f
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-08-19 08:45:41 -0300

    hello world

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hello world</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -16,7 +16,14 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+    <button onClick={gitHubAuth}> GitHub Login </button>
   </Layout>
 )
+
+const gitHubAuth = async () => {
+  console.log('Authorizing...')
+  window.open('https://github.com/login/oauth/authorize?client_id=Iv1.853babdc582ce5f0&redirect_uri=https://vtex.io&state=hashxpasoa')
+}
 
 export default IndexPage
